b30e99cc1c3a42ae1afdb90c5fe0c5c84056366f
pcpisquiy <pcpisquiy@gmail.com>
2021-05-10 11:03:08 -0600

    first commit

---
+++ Tipo_datos/Tipo_datos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tipo_datos
+{
+    class Program
+    {
+        #region  Variables numericas
+            int n1=0;
+            double dinero = 0.00;
+            decimal Dinero = 0;
+         #endregion
+#region Variables booleanas
+            bool bandera = true;
+#endregion 
+
+            #region variables de texto
+         
+            char letra = ' ';
+            string Cadena = "";
+            #endregion 
+
+            static void Main(string[] args)
+        {
+
+          
+
+            try 
+            {
+                Console.WriteLine();     
+            
+            }catch(Exception ex){
+                Console.WriteLine("ERROR: "+ ex.Message);
+            }
+        }
+    }
+}
